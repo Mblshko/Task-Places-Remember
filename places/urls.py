3f87e2baf5c0622831a5e86ae8c504bd94ade8be
Mblshko <mishka-007@yandex.ru>
2023-06-02 22:09:50 +0300

    Добавил список воспоминаний

--- places/urls.py
+++ places/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 
 from . import views
 
+
 urlpatterns = [
     path('', views.index, name='home'),
-    path('list/', views.places_list, name='list'),
+    path('list/', views.PlacesView.as_view(), name='list'),
     path('add/', views.place_add, name='add'),
 ]
--- places/views.py
+++ places/views.py
@@ -1,12 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import ListView
+
+from .models import Places
 
 
 def index(request):
     '''Главная страница'''
+    if request.user.is_authenticated:
+        return redirect('list')
     return render(request, 'places/index.html', {'title': 'Главная страница'})
 
-def places_list(request):
-    return render(request, 'places/list.html', {'title': 'Список мест'})
+
+class PlacesView(ListView):
+    model = Places
+    template_name = 'places/list.html'
+    context_object_name = 'places'
+
+    def get(self, request, *args, **kwargs):
+        self.object_list = self.get_queryset().filter(author=request.user)
+        return render(request, 'places/list.html', {'title': 'Воспоминания'})
+
 
 def place_add(request):
-    return render(request, 'places/add.html', {'title': 'Добавить место'})
+    return render(request, 'places/add.html', {'title': 'Добавить воспоминание'})
